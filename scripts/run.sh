a92727bc131bf84e39579328838e8ae54f8da212
kev-techi <kevin@techi.be>
2024-03-31 23:05:05 +0200

    add nvm

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,11 +22,13 @@ else
 
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+    export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
     source ~/.bashrc
 
     nvm install 21
-
-    source ~/.bashrc
 
     echo "Node installed"
 fi
