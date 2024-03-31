ff1a98dd7c72b4152b830b0084b5bfefd5dc7352
kev-techi <kevin@techi.be>
2024-03-31 23:02:42 +0200

    add nvm

--- scripts/run.sh
+++ scripts/run.sh
@@ -26,6 +26,10 @@ else
 
     nvm install 21
 
+    export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
     echo "Node installed"
 fi
 
