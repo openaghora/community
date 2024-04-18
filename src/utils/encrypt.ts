9a2e9f1558bc227ed97b7b4da98b6e95dbe0f88b
kev-techi <kevin@techi.be>
2024-04-18 21:49:06 +0200

    decrypt pk properly

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 export const encrypt = (secretValue: string, key: string): string => {
-  const keyBuffer = Buffer.from(key, "base64url");
+  const keyBuffer = Buffer.from(key, "base64");
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv("aes-256-cfb", keyBuffer, iv);
   const plaintext = Buffer.from(secretValue, "hex");
@@ -12,12 +12,12 @@ export const encrypt = (secretValue: string, key: string): string => {
 };
 
 export const decrypt = (encryptedValue: string, key: string): string => {
-  const keyBuffer = Buffer.from(key, "base64url");
-  const encryptedBuffer = Buffer.from(encryptedValue, "base64url");
+  const keyBuffer = Buffer.from(key, "base64");
+  const encryptedBuffer = Buffer.from(encryptedValue, "base64");
   const iv = encryptedBuffer.subarray(0, 16);
   const encryptedText = encryptedBuffer.subarray(16);
   const decipher = crypto.createDecipheriv("aes-256-cfb", keyBuffer, iv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString("hex");
+  return decrypted.toString("base64");
 };
