132fc970f0235a3f93ec5dfebf1db2fd67f8c2ce
kev-techi <kevin@techi.be>
2024-06-27 19:37:10 +0200

    clear node modules before installing again

--- scripts/run.sh
+++ scripts/run.sh
@@ -119,6 +119,8 @@ if [ "$CURRENT_VERSION" == "$NEW_VERSION" ]; then
 else
     echo "⏳ Community: installing..."
 
+    rm -rf community/node_modules > /dev/null 2>&1
+
     curl -o community.tar.gz -L "https://builds.internal.citizenwallet.xyz/community/dashboard_${NEW_VERSION}.tar.gz" > /dev/null 2>&1 & spinner
 
     tar -xzf community.tar.gz -C community > /dev/null 2>&1
@@ -126,8 +128,6 @@ else
     script_path="$HOME/community/scripts/boot.sh"
 
     chmod +x $script_path
-
-    rm -rf community/node_modules
 
     echo "✅ Community: installed"
 fi
